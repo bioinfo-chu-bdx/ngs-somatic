a2d24fe26be0ed7ecfeefd80f5d7f2e54a6f80c9
thoban <thomas.bandres@chu-bordeaux.fr>
2020-06-02 16:09:08 +0200

    tbandres-2020-06-02

---
+++ git-push.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+##########################################################
+confirm() {
+# syntax: confirm [<prompt>]
+# Prompts the user to enter Yes or No and returns 0/1.
+
+  local _prompt _default _response
+ 
+  if [ "$1" ]; then _prompt="$1"; else _prompt="Do you wish to continue?"; fi
+  _prompt="$_prompt [y/n] ?"
+ 
+  # Loop forever until the user enters a valid response (Y/N or Yes/No).
+  while true; do
+    read -r -p "$_prompt " _response
+    case "$_response" in
+      [Yy][Ee][Ss]|[Yy]) # Yes or Y (case-insensitive).
+        return 0
+        ;;
+      [Nn][Oo]|[Nn])  # No or N.
+        exit
+        ;;
+      *) # Anything else (including a blank) is invalid.
+        ;;
+    esac
+  done
+}
+##########################################################
+
+echo "- git add ...";
+git add -A;
+echo "- git status : ";
+git status;
+
+confirm;
+
+echo "git commit ...";
+git commit -m "$USER-`date '+%Y-%m-%d'`";
+echo "git push ...";
+git push -f origin master;
+
+# to pull : git pull origin master
